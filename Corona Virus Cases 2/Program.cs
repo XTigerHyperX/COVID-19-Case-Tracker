d0d3ca8a2eee24c693f57038bdb92f2dbf8463a6
Chris <psychopast.dev@gmail.com>
2020-12-27 00:33:22 +0200

    cleaned the code

--- Corona Virus Cases 2/Program.cs
+++ Corona Virus Cases 2/Program.cs
@@ -1,69 +1,82 @@
-﻿using System;
+using System;
 using System.Net;
-using Newtonsoft;
-using RestSharp;
-using Newtonsoft.Json;
+using System.Text.Json;
 
 namespace Corona_Virus_Cases_2
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             Console.Title = "COVID-19 Case tracker";
-            Start:
-            Console.ForegroundColor = ConsoleColor.White;
+            Utf8JsonReader jsonReader;
 
+        Start:
+            Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine("Enter Country Name ");
-            string a = Console.ReadLine();
+            string country = Console.ReadLine();
+            byte[] response;
             try
             {
-                RestClient Client = new RestClient("https://coronavirus-19-api.herokuapp.com/countries/");
-                IRestRequest JSONRequest = new RestRequest(a);
-                IRestResponse JSONResponse = Client.Execute(JSONRequest);
-                JSONResponse.Content = "[" + JSONResponse.Content + "]";
+                WebClient client = new();
+                response = client.DownloadData($"https://coronavirus-19-api.herokuapp.com/countries/{country}");
+            }
+            catch (WebException)
+            {
+                Console.WriteLine("The API is down. Try again later.");
+                return;
+            }
 
-                dynamic Data = JsonConvert.DeserializeObject(JSONResponse.Content);
-
-                foreach (dynamic ItemData in Data)
-                {
-
-                    Console.ForegroundColor = ConsoleColor.Magenta;
-
-                    Console.WriteLine("cases  " + ItemData.cases);
-                    Console.ForegroundColor = ConsoleColor.Cyan;
-
-                    Console.WriteLine("Cases Today : " + ItemData.todayCases);
-
-                    Console.ForegroundColor = ConsoleColor.Red;
-
-                    Console.WriteLine("Deaths : " + ItemData.deaths);
-                    Console.ForegroundColor = ConsoleColor.DarkRed;
-
-                    Console.WriteLine("Deaths Today :" + ItemData.todayDeaths);
-                    Console.ForegroundColor = ConsoleColor.Green;
-
-                    Console.WriteLine("Recovered  : " + ItemData.recovered);
-
-                    Console.ForegroundColor = ConsoleColor.DarkMagenta;
-
-                    Console.WriteLine("Critical  : " + ItemData.critical);
-                    Console.WriteLine("Total Tests  : " + ItemData.totalTests);
-
-
-                    Console.ForegroundColor = ConsoleColor.DarkYellow;
-
-
-                    Console.WriteLine("Active Cases: " + ItemData.active);
-
-                }
+            jsonReader = new(response);
+            try
+            {
+                jsonReader.Read();
+            }
+            catch (JsonException)
+            {
+                Console.WriteLine("An error has happened. Make sure you entered a right country name, else please contact @XTigerHyperX.");
                 goto Start;
             }
-            catch
-            {
-                Console.WriteLine("An Error Happened Make sure you entered a right country name ( starts with high case ) else Please contact @XTigerHyperX");
-                Console.ReadKey();
-            }
+
+            jsonReader.Read();
+            jsonReader.Read();
+            jsonReader.Read();
+            jsonReader.Read();
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine($"Cases: {jsonReader.GetInt32()}");
+            jsonReader.Read();
+            jsonReader.Read();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine($"Cases Today: {jsonReader.GetInt32()}");
+            jsonReader.Read();
+            jsonReader.Read();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"Deaths: {jsonReader.GetInt32()}");
+            jsonReader.Read();
+            jsonReader.Read();
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine($"Deaths Today: {jsonReader.GetInt32()}");
+            jsonReader.Read();
+            jsonReader.Read();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Recovered : {jsonReader.GetInt32()}");
+            jsonReader.Read();
+            jsonReader.Read();
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine($"Active Cases: {jsonReader.GetInt32()}");
+            jsonReader.Read();
+            jsonReader.Read();
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine($"Critical Cases: {jsonReader.GetInt32()}");
+            jsonReader.Read();
+            jsonReader.Read();
+            jsonReader.Read();
+            jsonReader.Read();
+            jsonReader.Read();
+            jsonReader.Read();
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine($"Total tests: {jsonReader.GetInt32()}");
+            goto Start;
         }
     }
 }

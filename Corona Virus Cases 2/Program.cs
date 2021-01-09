260c4b68592136dd678862f5a34067583b08415d
XTigerHyperX <xtigerhyperx@gmail.com>
2021-01-09 19:46:27 +0100

    test

--- Corona Virus Cases 2/Program.cs
+++ Corona Virus Cases 2/Program.cs
@@ -15,11 +15,13 @@ namespace Corona_Virus_Cases_2
             Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine("Enter Country Name ");
             string country = Console.ReadLine();
+            string endpoint = $"https://coronavirus-19-api.herokuapp.com/countries/{country}";
+
             byte[] response;
             try
             {
-                WebClient client = new();
-                response = client.DownloadData($"https://coronavirus-19-api.herokuapp.com/countries/{country}");
+                WebClient client = new WebClient();
+                response = client.DownloadData(endpoint);
             }
             catch (WebException)
             {
@@ -43,37 +45,37 @@ namespace Corona_Virus_Cases_2
             jsonReader.Read();
             jsonReader.Read();
             Console.ForegroundColor = ConsoleColor.Magenta;
-            Console.WriteLine($"Cases: {(jsonReader.TokenType == JsonTokenType.Number ? jsonReader.GetInt32() : null)}");
+            Console.WriteLine($"Cases: {(jsonReader.TokenType == JsonTokenType.Number ? jsonReader.GetInt32().ToString() : null)}");
 
             jsonReader.Read();
             jsonReader.Read();
             Console.ForegroundColor = ConsoleColor.Cyan;
-            Console.WriteLine($"Cases Today: {(jsonReader.TokenType == JsonTokenType.Number ? jsonReader.GetInt32() : null)}");
+            Console.WriteLine($"Cases Today: {(jsonReader.TokenType == JsonTokenType.Number ? jsonReader.GetInt32().ToString() : null)}");
 
             jsonReader.Read();
             jsonReader.Read();
             Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine($"Deaths: {(jsonReader.TokenType == JsonTokenType.Number ? jsonReader.GetInt32() : null)}");
+            Console.WriteLine($"Deaths: {(jsonReader.TokenType == JsonTokenType.Number ? jsonReader.GetInt32().ToString() : null)}");
 
             jsonReader.Read();
             jsonReader.Read();
             Console.ForegroundColor = ConsoleColor.DarkRed;
-            Console.WriteLine($"Deaths Today: {(jsonReader.TokenType == JsonTokenType.Number ? jsonReader.GetInt32() : null)}");
+            Console.WriteLine($"Deaths Today: {(jsonReader.TokenType == JsonTokenType.Number ? jsonReader.GetInt32().ToString() : null)}");
 
             jsonReader.Read();
             jsonReader.Read();
             Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine($"Recovered : {(jsonReader.TokenType == JsonTokenType.Number ? jsonReader.GetInt32() : null)}");
+            Console.WriteLine($"Recovered : {(jsonReader.TokenType == JsonTokenType.Number ? jsonReader.GetInt32().ToString() : null)}");
 
             jsonReader.Read();
             jsonReader.Read();
             Console.ForegroundColor = ConsoleColor.DarkMagenta;
-            Console.WriteLine($"Active Cases: {(jsonReader.TokenType == JsonTokenType.Number ? jsonReader.GetInt32() : null)}");
+            Console.WriteLine($"Active Cases: {(jsonReader.TokenType == JsonTokenType.Number ? jsonReader.GetInt32().ToString() : null)}");
 
             jsonReader.Read();
             jsonReader.Read();
             Console.ForegroundColor = ConsoleColor.DarkCyan;
-            Console.WriteLine($"Critical Cases: {(jsonReader.TokenType == JsonTokenType.Number ? jsonReader.GetInt32() : null)}");
+            Console.WriteLine($"Critical Cases: {(jsonReader.TokenType == JsonTokenType.Number ? jsonReader.GetInt32().ToString() : null)}");
 
             jsonReader.Read();
             jsonReader.Read();
@@ -82,7 +84,7 @@ namespace Corona_Virus_Cases_2
             jsonReader.Read();
             jsonReader.Read();
             Console.ForegroundColor = ConsoleColor.DarkYellow;
-            Console.WriteLine($"Total tests: {(jsonReader.TokenType == JsonTokenType.Number ? jsonReader.GetInt32() : null)}");
+            Console.WriteLine($"Total tests: {(jsonReader.TokenType == JsonTokenType.Number ? jsonReader.GetInt32().ToString() : null)}");
 
             goto Start;
         }
